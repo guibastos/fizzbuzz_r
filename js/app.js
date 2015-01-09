34dd51632afcedb3f61ff3c4fc6667c1471d3a1b
Gui Bastos <guibastos@gmail.com>
2015-01-08 22:25:11 -0800

    fixing functions

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
 $(document).ready(function() {
+
     function fizzBuzz () {
-        for (i = 1; i<=100; i++) {
-            if (i%3 == 0) {
-                if (i%5 == 0) {
-                    $('.list').append("<li>FizzBuzz</li>");
+        $('#errormessage').hide();
+        var input = $('#myInput').val();
+        var tr_input = $.trim(input); 
+        $('.output').remove();
+        
+        if (isNaN(tr_input) || (tr_input%1!==0)){
+            $('#errormessage').show();
+        }
+
+        else {
+
+            for (i = 1; i<=input; i++) {
+                if (i%3 == 0) {
+                    if (i%5 == 0) {
+                        $('.list').append("<li class='output'>FizzBuzz</li>");
+                    } 
+                    else {
+                        $('.list').append("<li class='output'>Fizz</li>");
+                    }
                 } 
-                else {
-                    $('.list').append("<li>Fizz</li>");
+                else if (i%5 == 0) {
+                    $('.list').append("<li class='output'>Buzz</li>");
                 }
-            } 
-            else if (i%5 == 0) {
-                $('.list').append("<li>Buzz</li>");
-            }
-            else {
-                $('.list').append("<li>"+ i +"</li>");
+                else {
+                    $('.list').append("<li class='output'>"+ i +"</li>");
+                }
             }
         }
-    };
-
+    }
+  
     $('#myButton').on('click', fizzBuzz);
-});
+    $('#myInput').on('keydown',function(e){
+        if(e.which==13) {fizzBuzz();}
+    });
+
+    });
